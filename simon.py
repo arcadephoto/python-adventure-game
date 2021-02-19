a249eb61e4fb6074f0b8a0c45844a7f691ee6bec
billysm64 <billysm64@gmail.com>
2021-02-19 13:43:56 -0500

    Improve Simon

--- simon.py
+++ simon.py
@@ -1,18 +1,24 @@
 import time
 import random
+from Game import Player
+from connect_four import Drop_game
 
-class Simon:
+class Simon(Player):
     def __init__(self):
-        print("You have stumbled across an insane evil scientist named Dr. Heinz Longnameovich, who is bent on understanding the workings of the human mind. He has kidnapped you so that he can undertake a psychological experiment on you. The scientist is hoping to find someone with the ability to get through 5 rounds of Simon without guessing something wrong, because such an individual would have the rare condition known as 'scolactomemorandizitis'. He ties you to a chair and makes you play the game, and if you win, you will get something very special. If you lose though, he kills you.\n\nDr. Longnameovich: Okay, here's how to play. Memorize each number I spurt out at you, and repeat them back to me, in order.\n")
+        print("You have stumbled across an insane evil scientist named Dr. Heinz Longnameovich, who is bent on understanding the workings of the human mind. He has kidnapped you so that he can undertake a psychological experiment on you. The scientist is hoping to find someone with the ability to get through 5 rounds of Simon without guessing something wrong, because such an individual would have the rare condition known as 'scolactomemorandizitis'. He ties you to a chair and makes you play the game, and if you win, you will get something very special. If you get a wrong answer, he shocks you with his machine and that takes away 2 health. If you lose though, he kills you.\n\nDr. Longnameovich: Okay, here's how to play. Memorize each number I spurt out at you, and repeat them back to me, in order.\n")
 
         start_game = input("Dr. Longnameovich: Press any button to begin. ")
 
         difficulty = 0
+        lost = False
         while 1:
             if difficulty == 5:
                 break
             else:
-                difficulty += 1
+                if not lost:
+                    difficulty += 1
+                else:
+                    lost = False
                 list_of_numbers = []
                 for enter in range(5000):
                     print("\n")
@@ -28,8 +34,11 @@ class Simon:
                     if int(prompt) == list_of_numbers[number]:
                         print("Dr. Longnameovich: Hmmm, very good!")
                     else:
-                        print("Dr. Longnameovich: Why, you got one wrong. Looks like you'll be useless to me. Say goodbye forever!")
-                        exit()
+                        print("Dr. Longnameovich: Why, you got one wrong. BZZZZZRRRTTT!!")
+                        lost = True
+                        health -= 2
+                        if health <= 0:
+                            Player.death()
                 list_of_numbers.clear()
         print("Dr. Longnameovich: Ah! I'm astounded! You actually won! Well, then, indeed I have learned a lot about the condition of scolactomemorandizitis. Here, I will give you this pearl. You may now be on your way!\n\n(Oh by the way, due to your condition and your age, you have 3 days left to live. Just a heads up...)")
 
