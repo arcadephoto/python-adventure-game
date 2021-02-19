139de010b20e8e3a330e85f7d53e3382e1d677b7
PatrickBoynton <jpboynton3@gmail.com>
2021-02-19 09:34:23 -0500

    Fixed the looping issue on game over.

--- Clue.py
+++ Clue.py
@@ -11,7 +11,6 @@ class Clue:
 
         self.suspect = self.suspects[randrange(1, 5)]
         self.murderer = self.suspects[randrange(1, 5)]
-        # self.murder_weapon = self.weapons[randrange(1, 5)]
         self.guesses = 3
         self.guess = []
 
@@ -20,7 +19,6 @@ class Clue:
 You hear a door close behind you as you open your eyes.
 You are in a room with 5 people. 
 It looks like they are having a party.
-These are the suspects: {self.suspects}
 What do you do?
               """)
 
@@ -36,11 +34,11 @@ What do you do?
         self.guess_the_suspect(player_input)
 
     def second_choice_text(self):
-        print("The people there are discussing a murder, who did it, "
+        print(f"The people there are discussing a murder, who did it, "
               "why, how that sort of thing. "
               "Turns out one of the people in that room did it."
               "Your job is to figure out who did it. "
-              "You have 3 guesses to do so.")
+              f"You have {self.guesses} guesses to do so.")
         print("These are the suspects: ")
         for i in self.suspects:
             print(i)
@@ -50,12 +48,12 @@ What do you do?
 
     def guess_the_suspect(self, player_input):
         while self.guesses > 0:
-            if player_input == str(1):
+            if player_input == "1":
                 self.game_over()
-            elif player_input == str(2):
+            elif player_input == "2":
                 self.show_suspects()
-        if self.suspect == self.murderer:
-            self.victory()
+            if self.suspect == self.murderer:
+                self.victory()
 
     def victory(self):
         print("Good job! You got the murderer, "
@@ -63,63 +61,23 @@ What do you do?
 
     def show_suspects(self):
         self.guesses -= 1
+        self.suspect = self.suspects[randrange(1, 5)]
         print(f"{self.guesses}", f"{self.suspect}", f"{self.murderer}")
 
     def game_over(self):
         self.guesses = 0
         print("Turns out the murderer was hanging out here, "
               "and decided to hit you in the head, and run away.")
+        return
 
     def area(self):
         player_input = input("1. Leave the house.\n2. Join the party\n")
 
-        while True:
-
+        while self.guesses > 0:
             if player_input == str(1):
                 self.first_choice()
             elif player_input == str(2):
                 self.second_choice()
-                break
-
-            #     # TODO convert logic into method.
-            # elif player_input == str(2):
-            #     print("They are talking about someone who died recently"
-            #           " that do you want to do? There is a die on the table.")
-            #
-            #     player_input = input("1. roll die, 2. Hang out.\n")
-            #
-            #     if player_input == str(1):
-            #         #     TODO roll die
-            #         die_1_roll = randrange(1, 5)
-            #         die_2_roll = randrange(1, 5)
-            #         die_3_roll = randrange(1, 4)
-            #
-            #         print(self.suspects[die_1_roll], self.weapons[die_2_roll],
-            #               self.suspect)
-            #         guesses = 3
-            #         while guesses > 0:
-            #             if self.suspect == self.suspects[die_1_roll]:
-            #                 print(f"{self.suspect[die_1_roll]} "
-            #                       f"turned out to be the culprit! "
-            #                       "Quick call the police and have "
-            #                       "him/her arrested!"
-            #                       "He has your missing [item], "
-            #                       "and you can now use it to leave.")
-            #             # break
-            #         else:
-            #             guesses -= 1
-            #             print(f"You were wrong! {self.suspects[die_1_roll]} "
-            #                   f"was not the murderer.")
-            #
-            #         del self.suspects[die_1_roll]
-            #         self.suspect = self.suspects[die_1_roll]
-            #
-            #     elif player_input == str(2):
-            #         print("While hanging out you find out that the person who "
-            #               "died was killed by someone in the group. "
-            #               "You need to solve who did it in order to find your"
-            #               "missing items.")
-            #     break
 
 
 game = Clue()
